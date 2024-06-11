d238cd54ca74f0db1dddfd5538dff4c68eb18df4
Amirhossein Rahmanzadeh <rahmanzadeh.99@gmail.com>
2024-06-11 16:55:08 +0200

    fix the issue of not returning response from the service catalog

--- Microservices/catalog_expose/service_service.py
+++ Microservices/catalog_expose/service_service.py
@@ -2,33 +2,38 @@ import cherrypy
 import json
 import datetime
 
+
 class ServiceCatalogExpose:
     exposed = True
+
     def __init__(self, data):
         self.serviceCatalog = data
-        
+
     @cherrypy.tools.json_out()
     def GET(self, *args, **kwargs):
         if args:
-            if(args[0] == 'topics'):
-                return self.serviceCatalog.topics
-            elif(args[0] == 'services'):
-                return self.serviceCatalog.services
-            elif(args[0] == 'broker'):
-                return self.serviceCatalog.broker
-            elif(args[0] == 'all'):
-                return self.serviceCatalog
-        
+            key = args[0]
+            if key in self.serviceCatalog:
+                return self.serviceCatalog[key]
+            else:
+                if key.lower() == 'all':
+                    return self.serviceCatalog
+                else:
+                    raise cherrypy.HTTPError(
+                        404, "Key not found: {}".format(key))
+        else:
+            return self.serviceCatalog
+
 
 if __name__ == '__main__':
     with open('./service_catalog.json', 'r') as file:
         data = json.load(file)
         serviceCatalog = ServiceCatalogExpose(data)
         conf = {
-        '/':{
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.sessions.on' : True
-        }
+            '/': {
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+                'tools.sessions.on': True
+            }
         }
         cherrypy.config.update({
             'server.socket_host': '0.0.0.0',
