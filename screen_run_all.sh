f1ef6360ae6b8460922b63519525d11f3f19e488
dagh3n <ciaosamurame@gmail.com>
2024-08-09 16:47:56 +0200

    script to run all debug microservices

---
+++ screen_run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pkill screen
+
+screen -S resource_catalog -dm bash -c 'source venv/bin/activate; python Microservices/catalog_expose/resource_service.py'
+
+screen -S service_catalog -dm bash -c 'source venv/bin/activate; python Microservices/catalog_expose/service_service.py'
+
+screen -S telegram_bot -dm bash -c 'source venv/bin/activate; python TelegramBot/bot.py'
+
+sleep 5
+
+screen -S recommendation_service -dm bash -c 'source venv/bin/activate; python Microservices/recommendation_service/main.py'
+
+screen -S vase_control -dm bash -c 'source venv/bin/activate; python Microservices/vase_control/main.py'
+
+screen -S image_recognition -dm bash -c 'source venv/bin/activate; python Microservices/image_recognition/main.py'
+
+
+wait
